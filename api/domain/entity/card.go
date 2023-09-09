7a96d082b2de3c555090b158f416cfb7059dd726
rare0b <rare0b0110@gmail.com>
2023-09-09 15:26:47 +0900

    feat: カラム追加 GPTの文章(AnalysisResult)とそこから抽出したタグ(Tags)

--- api/domain/entity/card.go
+++ api/domain/entity/card.go
@@ -1,16 +1,20 @@
 package entity
 
-import "time"
+import (
+	"github.com/lib/pq"
+	"time"
+)
 
 type Card struct {
-	CardID  string `json:"card_id" gorm:"primaryKey;size:255;default:uuid_generate_v4()"`
-	UserID  string `json:"user_id" gorm:"not null;index"`
-	Period  string `json:"period" gorm:"not null"`
-	Title   string `json:"title" gorm:"not null"`
-	Content string `json:"content" gorm:""`
-	//Tags      string    `json:"tags" gorm:""` // TODO: []stringだとエラーになる
-	CreatedAt time.Time `json:"created_at" gorm:"not null"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"not null"`
-	DeletedAt time.Time `json:"deleted_at" gorm:""`
-	User      User      `gorm:"references:UserID"`
+	CardID         string         `json:"card_id" gorm:"primaryKey;size:255;default:uuid_generate_v4()"`
+	UserID         string         `json:"user_id" gorm:"not null;index"`
+	Period         string         `json:"period" gorm:"not null"`
+	Title          string         `json:"title" gorm:"not null"`
+	Content        string         `json:"content" gorm:""`
+	AnalysisResult string         `json:"analysis_result" gorm:""`
+	Tags           pq.StringArray `json:"tags" gorm:"type:text[]"`
+	CreatedAt      time.Time      `json:"created_at" gorm:"not null"`
+	UpdatedAt      time.Time      `json:"updated_at" gorm:"not null"`
+	DeletedAt      time.Time      `json:"deleted_at" gorm:""`
+	User           User           `gorm:"references:UserID"`
 }
