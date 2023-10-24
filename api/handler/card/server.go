f31b3f7effbb556d87cc06654b086ddc86e0817e
rare0b <rare0b0110@gmail.com>
2023-10-24 23:18:25 +0900

    wip

--- api/handler/card/server.go
+++ api/handler/card/server.go
@@ -2,9 +2,10 @@ package card
 
 import (
 	"github.com/Seiya-Tagami/Recollect-Service/api/domain/entity"
+	"github.com/Seiya-Tagami/Recollect-Service/api/handler/util/myerror"
 	"github.com/Seiya-Tagami/Recollect-Service/api/response"
 	"github.com/Seiya-Tagami/Recollect-Service/api/usecase/card"
-	"github.com/Seiya-Tagami/Recollect-Service/api/utils"
+	"github.com/Seiya-Tagami/Recollect-Service/api/util"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"net/http"
@@ -27,11 +28,16 @@ func New(cardInteractor card.Interactor) Handler {
 }
 
 func (h *handler) ListCards(c *gin.Context) {
-	userID := userIDFromToken(c)
+	userID, err := userIDFromToken(c)
+	if err != nil {
+		myerror.HandleError(c, err)
+		return
+	}
 
 	cards, err := h.cardInteractor.ListCards(userID)
 	if err != nil {
-		panic(err)
+		myerror.HandleError(c, err)
+		return
 	}
 
 	cardsResponse := response.ToCardsResponse(&cards)
@@ -40,17 +46,23 @@ func (h *handler) ListCards(c *gin.Context) {
 }
 
 func (h *handler) CreateCard(c *gin.Context) {
-	userID := userIDFromToken(c)
+	userID, err := userIDFromToken(c)
+	if err != nil {
+		myerror.HandleError(c, err)
+		return
+	}
 
 	cardReq := entity.Card{}
 	if err := c.BindJSON(&cardReq); err != nil {
-		panic(err)
+		myerror.HandleError(c, err)
+		return
 	}
 	cardReq.UserID = userID
 
 	card, err := h.cardInteractor.CreateCard(cardReq)
 	if err != nil {
-		panic(err)
+		myerror.HandleError(c, err)
+		return
 	}
 
 	cardResponse := response.ToCardResponse(&card)
@@ -64,10 +76,15 @@ func (h *handler) CreateCards(c *gin.Context) {
 	}
 	var batchReq BatchReq
 
-	userID := userIDFromToken(c)
+	userID, err := userIDFromToken(c)
+	if err != nil {
+		myerror.HandleError(c, err)
+		return
+	}
 
 	if err := c.BindJSON(&batchReq); err != nil {
-		panic(err)
+		myerror.HandleError(c, err)
+		return
 	}
 
 	cardsReq := batchReq.Cards
@@ -77,7 +94,8 @@ func (h *handler) CreateCards(c *gin.Context) {
 
 	cards, err := h.cardInteractor.CreateCards(cardsReq)
 	if err != nil {
-		panic(err)
+		myerror.HandleError(c, err)
+		return
 	}
 
 	cardsResponse := response.ToCardsResponse(&cards)
@@ -87,11 +105,16 @@ func (h *handler) CreateCards(c *gin.Context) {
 
 func (h *handler) UpdateCard(c *gin.Context) {
 	id := c.Param("id")
-	userID := userIDFromToken(c)
+	userID, err := userIDFromToken(c)
+	if err != nil {
+		myerror.HandleError(c, err)
+		return
+	}
 
 	cardReq := entity.Card{}
 	if err := c.BindJSON(&cardReq); err != nil {
-		panic(err)
+		myerror.HandleError(c, err)
+		return
 	}
 
 	cardReq.CardID = id
@@ -99,7 +122,8 @@ func (h *handler) UpdateCard(c *gin.Context) {
 
 	card, err := h.cardInteractor.UpdateCard(cardReq, id)
 	if err != nil {
-		panic(err)
+		myerror.HandleError(c, err)
+		return
 	}
 
 	cardResponse := response.ToCardResponse(&card)
@@ -112,21 +136,27 @@ func (h *handler) DeleteCard(c *gin.Context) {
 
 	err := h.cardInteractor.DeleteCard(id)
 	if err != nil {
-		panic(err)
+		myerror.HandleError(c, err)
+		return
 	}
 
 	c.Status(http.StatusNoContent)
 }
 
-func userIDFromToken(c *gin.Context) string {
+func userIDFromToken(c *gin.Context) (string, error) {
 	tokenString, err := c.Cookie("user_token")
 	if err != nil {
-		panic(err)
+		myerror.HandleError(c, err)
+		return "", myerror.InvalidRequest
 	}
-	token, err := utils.ParseToken(tokenString)
+	token, err := util.ParseToken(tokenString)
+	if token == nil || err != nil {
+		return "", myerror.InvalidRequest
+	}
+
 	var userID string
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		userID = claims["user_id"].(string)
 	}
-	return userID
+	return userID, nil
 }
